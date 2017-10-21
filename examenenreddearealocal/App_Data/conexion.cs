e3e09d9c653f70a0075f09f74a94aa96b6e57152
jeanrhe <jeanrhe@gmail.com>
2017-10-21 10:58:40 -0500

    Inicio del Proyecto

---
+++ examenenreddearealocal/App_Data/conexion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace clase77
+{
+    public class conexion
+    {
+        public SqlConnection cadena = new SqlConnection("Data Source=.;Initial Catalog=neptuno;Integrated Security=True");
+        public void conectar() {
+            cadena.Open();
+        }
+        public void cerrar() {
+            cadena.Close();
+        }
+    }
+}
---
+++ examenenreddearealocal/App_Data/hejconsultar.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace clase77
+{
+    public class gsConsultar
+    {
+        conexion cn = new conexion();
+        public DataTable extrae( string nombresp)
+        {
+            DataTable dt = new DataTable();
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            cmd.Connection = cn.cadena;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = nombresp;
+            cn.conectar();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            cn.cerrar();
+            return dt;
+        }
+
+        public DataTable extrae(string nombresp, string nomparam, string valorparam)
+        {
+            DataTable dt = new DataTable();
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            cmd.Connection = cn.cadena;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(nomparam, SqlDbType.VarChar).Value = valorparam;
+            cmd.CommandText = nombresp;
+            cn.conectar();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            cn.cerrar();
+            return dt;
+        }
+
+
+        public DataTable extrae(string nombresp, string nomparam, int valorparam)
+        {
+            DataTable dt = new DataTable();
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            cmd.Connection = cn.cadena;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(nomparam, SqlDbType.Int).Value = valorparam;
+            cmd.CommandText = nombresp;
+            cn.conectar();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            cn.cerrar();
+            return dt;
+        }
+
+
+        public DataTable extrae(string nombresp, string nomparam1, string valorparam1, 
+                                   string nomparam2, string valorparam2)
+        {
+            DataTable dt = new DataTable();
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            cmd.Connection = cn.cadena;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(nomparam1, SqlDbType.VarChar).Value = valorparam1;
+            cmd.Parameters.Add(nomparam2, SqlDbType.VarChar).Value = valorparam2;
+            cmd.CommandText = nombresp;
+            cn.conectar();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            cn.cerrar();
+            return dt;
+        }
+
+
+        public void rellenacombo(DropDownList cmb, string campover, string campovalor, string nombresp)
+        {
+            cmb.DataSource = extrae(nombresp);
+            cmb.DataValueField = campovalor;
+            cmb.DataTextField = campover;
+            cmb.DataBind();
+        }
+
+    }
+}
